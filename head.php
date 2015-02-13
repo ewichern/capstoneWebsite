73f0f0a0f208bfa62518a56a9a7c341bdf5972b1
410 green <410green@cs.odu.edu>
2015-02-13 14:37:50 -0500

    Initial commit.

---
+++ head.php
@@ -0,0 +1,9 @@
+<?php
+echo'
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/bootstrap.css"/>
+<link rel="stylesheet" href="css/starter-template.css"/>
+';
+?>
