6e1ef690405b0023e6f252abdb3728dad44b7171
ewichern <ewichern@cs.odu.edu>
2015-02-15 23:11:55 -0500

    Fixed broken link.

--- navbar.php
+++ navbar.php
@@ -11,7 +11,7 @@ echo'
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="index.phtml">cs410green</a>
+			<a class="navbar-brand" href="index.php">cs410green</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
