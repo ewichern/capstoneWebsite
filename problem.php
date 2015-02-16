5703d101631b3d72edc5c1d2875c6c04d3444191
diamon wiggins <dwiggins@cs.odu.edu>
2015-02-15 23:54:24 -0500

    Added problem description and graphic

--- problem.php
+++ problem.php
@@ -10,8 +10,16 @@
 		<div class="container">
 
 			<div class="starter-template">
-				<h1>The Problem</h1>
-				<p class="lead">Many people in the city of Norfolk are uninformed about the far reaching implications of climate change in their own community.<br/>
+				<h1>Problem Description</h1>
+				<p class="lead">There is an absence of captivating climate change journalism in Norfolk, Virginia.</br></br>
+					Characteristics of the Problem
+					<ul>
+						<li>Local news outlets have minimal coverage of climate change.</li>
+						<li>Stories about climate change aren't being presented in a way that captures the hearts and minds of readers.</li>
+						<li>Data visualization is underutilized for demonstrating the implications of climate change here in Norfolk.</li></br>
+					</ul>
+				<img src="./images/flow_chart.png" class="img-responsive"></br>
+					The last step in the above process flow diagram is where intervention is needed. As it stands right now, the Mace and Crown doesn't have the resources to publish their climate change stories in a way that grabs people's attention. These stories need to immerse the readers in the subject matter, and that would require improving upon the way it's presented.
 				</p>
 			</div>
 
