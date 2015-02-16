a865e3ca44a6323e603e85dd08479eeaf0c8fdba
410green <410green@cs.odu.edu>
2015-02-15 23:00:13 -0500

    Changed extensions back to php so they work on oducs servers.

---
+++ references.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>cs410green References</title>
+		<?php require 'head.php'; ?>
+	</head>
+	<body>
+		<?php require 'navbar.php'; ?>
+
+		<div class="container">
+
+			<div class="starter-template">
+				<h1>References</h1>
+				<p class="lead">
+				Environmental Protection Agency - Climate Change: Basic Information</br>
+				<a href="http://www.epa.gov/climatechange/basics/">http://www.epa.gov/climatechange/basics</a></br></br>
+				2014 National Climate Assessment</br>
+				<a href="http://nca2014.globalchange.gov/report">http://nca2014.globalchange.gov/report</a></br></br>
+				Natural Resources Defense Council - Climate Change Health Threats in Virginia</br>
+				<a href="http://www.nrdc.org/health/climate/va.asp">http://www.nrdc.org/health/climate/va.asp</a></br></br>				
+				Washington Post - In Norfolk, evidence of climate change is in the streets at high tide</br>
+				<a href="http://www.washingtonpost.com/business/economy/in-norfolk-evidence-of-climate-change-is-in-the-streets-at-high-tide/2014/05/31/fe3ae860-e71f-11e3-8f90-73e071f3d637_story.html">http://www.washingtonpost.com/business/economy/in-norfolk-evidence-of-climate-change-is-in-the-streets-at-high-tide/2014/05/31/fe3ae860-e71f-11e3-8f90-73e071f3d637_story.html</a></br></br>
+				NPR - As Sea Levels Rise, Norfolk Is Sinking And Planning</br>
+				<a href="http://www.npr.org/2014/06/24/324891517/as-sea-levels-rise-norfolk-is-sinking-and-planning">http://www.npr.org/2014/06/24/324891517/as-sea-levels-rise-norfolk-is-sinking-and-planning</a></br></br>
+				USA Today - Rising sea levels torment Norfolk, Va., and coastal U.S.</br>
+				<a href="http://www.usatoday.com/story/news/nation/2013/12/17/sea-level-rise-swamps-norfolk-us-coasts/3893825/">http://www.usatoday.com/story/news/nation/2013/12/17/sea-level-rise-swamps-norfolk-us-coasts/3893825/</a></br></br>
+				Huffington Post - Norfolk, Virginia's Rising Sea Levels And Sinking Ground Pose A Community Challenge</br>
+				<a href="http://www.huffingtonpost.com/2012/04/28/norfolk-virginia-sea-levels-rise_n_1461486.html">http://www.huffingtonpost.com/2012/04/28/norfolk-virginia-sea-levels-rise_n_1461486.html</a></br></br>
+				The Daily Climate - Sea-level rise: Whatever you call it, water is rising in Norfolk</br>
+				<a href="http://www.dailyclimate.org/tdc-newsroom/2012/08/norfolk-sea-level-rise">http://www.dailyclimate.org/tdc-newsroom/2012/08/norfolk-sea-level-rise</a></br></br>
+				</p>
+			</div>
+
+		</div><!-- /.container -->
+
+		<?php require 'scripts.php'; ?>
+	</body>
+</html>
