ada4d7ef2e983a27b922cac1c99153529cf77f50
ewichern <ewichern@cs.odu.edu>
2015-02-16 00:09:32 -0500

    Added footer to each page.

---
+++ foot.php
@@ -0,0 +1,11 @@
+<?php
+echo'
+<br>
+<div class="btn-group">
+	<a href="index.php" class="btn btn-success">410green Home</a>
+	<a href="bios.php" class="btn btn-success">About the Team</a>
+	<a href="problem.php" class="btn btn-success">Problem Description</a>
+	<a href="references.php" class="btn btn-success">References</a>
+</div>	
+';
+?>
--- problem.php
+++ problem.php
@@ -13,6 +13,9 @@
 				<h1>The Problem</h1>
 				<p class="lead">Many people in the city of Norfolk are uninformed about the far reaching implications of climate change in their own community.<br/>
 				</p>
+
+				<?php include 'foot.php'; ?>
+
 			</div>
 
 		</div><!-- /.container -->
