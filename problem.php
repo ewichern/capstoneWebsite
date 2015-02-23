40c2e77f4ea9a66ef5b3086a298318809dcccc89
ewichern <ewichern@cs.odu.edu>
2015-02-22 20:26:02 -0500

    Partially implemented expanding panels with problem characteristics.

--- problem.php
+++ problem.php
@@ -10,24 +10,68 @@
 		<div class="container">
 
 			<div class="starter-template">
-				<h1>Problem Description</h1>
-				<p class="lead">As digital journalism and data visualization grows in in popularity the technological skillset of undergraduate journalists is inadequate for the demands of university news outlets preparing content for publication in a competitive news arena.</p>
+				<h3>Problem Description</h3>
+				<br/>
+				<div class="panel panel-success">
+					<div class="panel-heading" id="statementHeading">
+						<h3 class="panel-title">
+							<!-- <a data-toggle="collapse" href="#collapseStatement" aria-expanded="false" aria-controls="collapseStatement"></a> -->
+							Problem Statement
+						</h3>
+					</div>
+					<!-- <div id="collapseStatement" class="panel-collapse collapse" role="tabpanel" aria-labelledby="statementHeading"> -->
+					<div class="panel-body" align="left">
+						As digital journalism and data visualization grows in in popularity the technological skillset of undergraduate journalists is inadequate for the demands of university news outlets preparing content for publication in a competitive news arena.
+					</div>
+					<!-- </div> -->
+				</div>
 
-				<p class="lead">-- Remainder of page requires revision and is under construction --</p>
-				<del>
-					<p class="lead">Characteristics of the Problem</p>
+				<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+					<!-- <a href="" class="btn btn-link" role="button"></a> -->
 
-					<ul class="text-left">
-						<li>Local news outlets have minimal coverage of climate change.</li>
-						<li>Stories about climate change aren't being presented in a way that captures the hearts and minds of readers.</li>
-						<li>Data visualization is underutilized for demonstrating the implications of climate change here in Norfolk.</li></br>
-					</ul>
-				</del>
-				<del>
+					<div class="panel panel-success">
+						<div class="panel-heading" role="tab" id="headingOne">
+							<h4 class="panel-title" align="left">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+									<span class="glyphicon glyphicon-unchecked" aria-hidden="true"></span> University newspapers must maintain existing operations and expand digital presence on limited budgets and face declining revenue from print advertising. Online advertising “cannot make up for the loss of print revenue.”
+								</a>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+							<div class="panel-body" align="left">
+								‘“All newspapers have these problems,” Eric Fidler, the Daily Egyptian’s faculty adviser, said. “Replacing print dollars with digital dollars doesn’t work. You can’t make enough money online to make up for the loss of print revenue.”’
+								<a href="http://www.stltoday.com/news/local/education/student-newspapers-weigh-trading-independence-for-financial-support/article_29150c79-624f-5527-9fa1-505faa3e5b5b.html" class="btn btn-link" role="button">-- Student newspapers weigh trading independence for financial support</a>
+								<br/><br/>
+								“At any given time, the paper employs 30 or more editors, writers, advertising reps, photographers, copy editors and graphic designers. About half of the staff is paid.” 
+								<a href="http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html" class="btn btn-link" role="button">-- Read All About It ... In The Student Paper</a>
+								<br/><br/>
+								“MC with $32,380” -- Mace & Crown budget for 2014-2015.
+								<a href="http://www.maceandcrown.com/2014/09/03/sga-balances-600-thousand-in-student-organization-budgets/" class="btn btn-link" role="button">-- SGA balances behemoth budget</a>
+							</div>
+						</div>
+					</div>
 
-					<img src="./images/flow_chart.png" class="img-responsive"/><br/>
+					<div class="panel panel-success">
+						<div class="panel-heading" role="tab" id="headingTwo">
+							<h4 class="panel-title" align="left">
+								<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+									<span class="glyphicon glyphicon-unchecked" aria-hidden="true"></span> Top-tier media outlets are employing entire teams focused on digital journalism and interactive media. To remain competitive in this environment requires a team competent in skills that cross many disciplines. Journalism students do not receive training in complex information/data visualization.
+								</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							<div class="panel-body" align="left">
+								Large media outlets are utilizing entire teams of employees focused on digital and interactive media. 
+								<a href="https://www.journalism.co.uk/news/10-key-skills-for-digital-journalists-to-hone-in-2014/s2/a555503/" class="btn btn-link" role="button">-- 10 key skills for digital journalists to hone in 2014</a>
+							</div>
+						</div>
+					</div>
 
-					<p class="text-left">The last step in the above process flow diagram is where intervention is needed. As it stands right now, the Mace and Crown doesn't have the resources to publish their climate change stories in a way that grabs people's attention. These stories need to immerse the readers in the subject matter, and that would require improving upon the way it's presented.</p>
+				</div>
+				<br/>
+				<img src="./images/flow_chart.png" class="img-responsive"/><br/>
+
+				<p class="text-left">The last step in the above process flow diagram is where intervention is needed. As it stands right now, the Mace and Crown doesn't have the resources to publish their climate change stories in a way that grabs people's attention. These stories need to immerse the readers in the subject matter, and that would require improving upon the way it's presented.</p>
 				</del>
 
 				<?php include 'foot.php'; ?>
