a54e17486d6c0db005d8be2c86580b7cc0a6c47b
jperez757 <Johnathan@jap.home>
2015-03-29 21:50:12 -0400

    working on table

--- risk_matrix.php
+++ risk_matrix.php
@@ -17,7 +17,7 @@
 							<td><h4>Probability</h4><td>
 						<tr>
 						<tr>
-							<td class="vertical-text" style="width: 20px; border: none">
+							<td class="vertical-text" style="width: 20px; border: none; transform-origin: left top 0;">
 								<h4>Impact</h4>
 							</td>
 							<td>
