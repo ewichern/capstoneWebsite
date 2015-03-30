fe21f25ddb5230d2f7a61dc495187fbdeaafa219
jperez757 <Johnathan@jap.home>
2015-03-29 23:11:53 -0400

    working on table

--- risk_matrix.php
+++ risk_matrix.php
@@ -9,77 +9,82 @@
 
 		<div class="container">
 			<div class="row">
-				<h1>Risk Matrix</h1>
-				<table class="table">
-					<tbody>
-						<tr>
-							<td><h4>Probability</h4><td>
-						<tr>
-						<tr>
-							<td class="vertical-text" style="width: 20px; border: none; transform:rotate(270deg);">
-								<h4>Impact</h4>
-							</td>
-							<td>
-								<div class="table-responsive">
-									<table class="table table-bordered">
-										<tbody>
-											<tr>
-												<th class"col-xs-1"></th>
-												<th style="text-align: center" class="col-sm-2">1</th>
-												<th style="text-align: center" class="col-sm-2">2</th>
-												<th style="text-align: center" class="col-sm-2">3</th>
-												<th style="text-align: center" class="col-sm-2">4</th>
-												<th style="text-align: center" class="col-sm-2">5</th>
-											</tr>
-											<tr>
-												<td align="center">5</td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="orange"></td>
-												<td align="center" bgcolor="red"></td>
-												<td align="center" bgcolor="red"></td>
-											</tr>
-											<tr>
-												<td align="center">4</td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="orange"></td>
-												<td align="center" bgcolor="red"></td>
-											</tr>
-											<tr>
-												<td align="center">3</td>
-												<td align="center" bgcolor="greenyellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="orange"></td>
-											</tr>
-											<tr>
-												<td align="center">2</td>
-												<td align="center" bgcolor="green"></td>
-												<td align="center" bgcolor="greenyellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-											</tr>
-											<tr>
-												<td align="center">1</td>
-												<td align="center" bgcolor="green"></td>
-												<td align="center" bgcolor="green"></td>
-												<td align="center" bgcolor="greenyellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-												<td align="center" bgcolor="yellow"></td>
-											</tr>
-										</tbody>
-									</table>
-								</div>
-							</td>
-						</tr>
-					</tbody>
-				</table>
+				<div class="panel panel-success">
+						<div class="panel-heading" id="CustomerRisksHeading">
+								<h3 class="panel-title">
+										Risk Matrix
+								</h3>
+						</div>
+					<table class="table">
+						<tbody>
+							<tr>
+								<td><h4>Probability</h4><td>
+							<tr>
+							<tr>
+								<td class="vertical-text" style="width: 20px; border: none; transform:rotate(270deg);">
+									<h4>Impact</h4>
+								</td>
+								<td>
+									<div class="table-responsive">
+										<table class="table table-bordered">
+											<tbody>
+												<tr>
+													<th class"col-xs-1"></th>
+													<th style="text-align: center" class="col-sm-2">1</th>
+													<th style="text-align: center" class="col-sm-2">2</th>
+													<th style="text-align: center" class="col-sm-2">3</th>
+													<th style="text-align: center" class="col-sm-2">4</th>
+													<th style="text-align: center" class="col-sm-2">5</th>
+												</tr>
+												<tr>
+													<td align="center">5</td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="orange"></td>
+													<td align="center" bgcolor="red"></td>
+													<td align="center" bgcolor="red"></td>
+												</tr>
+												<tr>
+													<td align="center">4</td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="orange"></td>
+													<td align="center" bgcolor="red"></td>
+												</tr>
+												<tr>
+													<td align="center">3</td>
+													<td align="center" bgcolor="greenyellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="orange"></td>
+												</tr>
+												<tr>
+													<td align="center">2</td>
+													<td align="center" bgcolor="green"></td>
+													<td align="center" bgcolor="greenyellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+												</tr>
+												<tr>
+													<td align="center">1</td>
+													<td align="center" bgcolor="green"></td>
+													<td align="center" bgcolor="green"></td>
+													<td align="center" bgcolor="greenyellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+													<td align="center" bgcolor="yellow"></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+			</div>
 
-				<hr class="featurette-divider">
 				<div class="panel panel-success">
 						<div class="panel-heading" id="CustomerRisksHeading">
 								<h3 class="panel-title">
