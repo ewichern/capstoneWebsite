85dd962b8938ae5083bd12002796836dd40f68bf
jperez757 <Johnathan@new-host-11.home>
2015-04-05 21:56:37 -0400

    adding user stories to the website

--- deliverableFiles.php
+++ deliverableFiles.php
@@ -14,11 +14,20 @@
 						<h3 class="panel-title">Feasibility Presentation</h3>
 					</div>
 					<div class="panel-body" align="left" id="feasibilityBody">
-					<a href="./images/FeasibilityPresentationOriginal.pdf" class="btn btn-link" role="button">Feasibility Presentation (Original)</a>
-					<br>
-					<a href="./images/FeasibilityPresentationRevised.pdf" class="btn btn-link" role="button">Feasibility Presentation (Revised)</a>
-					<br>
-					<a href="./images/FeasibilityPresentationCurrent.pdf" class="btn btn-link" role="button">Feasibility Presentation</a>
+						<a href="./images/FeasibilityPresentationOriginal.pdf" class="btn btn-link" role="button">Feasibility Presentation (Original)</a>
+						<br>
+						<a href="./images/FeasibilityPresentationRevised.pdf" class="btn btn-link" role="button">Feasibility Presentation (Revised)</a>
+						<br>
+						<a href="./images/FeasibilityPresentationCurrent.pdf" class="btn btn-link" role="button">Feasibility Presentation</a>
+						</div>
+				</div>
+
+				<div class="panel panel-success">
+					<div class="panel-heading" id="userStoriesHeading">
+						<h3 class="panel-title">User Stories</h3>
+					</div>
+					<div class="panel-body" align="left" id="userStoriesBody">
+						<a href="./images/UserStories.pdf" class="btn btn-link" role="button">User Stories</a>
 					</div>
 				</div>
 
