69b736211e6d618f9955783f921f9e4f17a981c4
410green <410green@cs.odu.edu>
2015-04-13 00:46:26 -0400

    Fixed footer alignment.

--- foot.php
+++ foot.php
@@ -1,17 +1,19 @@
 <?php
 echo'
 <br>
-<div class="btn-group" role="group" aria-label="footer navigation buttons" align="center">
-	<a href="index.php" class="btn btn-primary">410green Home</a>
-	<a href="bios.php" class="btn btn-primary">About the Team</a>
-	<a href="problem.php" class="btn btn-primary">Problem Description</a>
-	<a href="solution.php" class="btn btn-primary">Solution Characteristics</a>
-</div>
-<div class="btn-group" role="group" aria-label="footer navigation buttons" align="center">
-	<a href="risk_matrix.php" class="btn btn-primary">Risk Matrix</a>
-	<a href="deliverableFiles.php" class="btn btn-primary">Deliverable Files</a>
-	<a href="references.php" class="btn btn-primary">References</a>
-	<a href="glossary.php" class="btn btn-primary">Glossary</a>
+<div class="text-center">
+	<div class="btn-group" role="group" aria-label="footer navigation buttons" align="center">
+		<a href="index.php" class="btn btn-primary">410green Home</a>
+		<a href="bios.php" class="btn btn-primary">About the Team</a>
+		<a href="problem.php" class="btn btn-primary">Problem Description</a>
+		<a href="solution.php" class="btn btn-primary">Solution Characteristics</a>
+	</div>
+	<div class="btn-group" role="group" aria-label="footer navigation buttons" align="center">
+		<a href="risk_matrix.php" class="btn btn-primary">Risk Matrix</a>
+		<a href="deliverableFiles.php" class="btn btn-primary">Deliverable Files</a>
+		<a href="references.php" class="btn btn-primary">References</a>
+		<a href="glossary.php" class="btn btn-primary">Glossary</a>
+	</div>
 </div>
 ';
 ?>
