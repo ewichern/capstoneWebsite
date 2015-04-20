37af62ba443aef88aac60be27a654ae5d5e4c109
jperez757 <Johnathan@new-host-7.home>
2015-04-20 03:57:34 -0400

    linked algorithms to the display page

--- deliverableFiles.php
+++ deliverableFiles.php
@@ -51,7 +51,7 @@
                         <img src="./images/SiteMap.png" class="img-responsive"/>
                     </div>
                 </div>
-                
+
                 <div class="panel panel-primary">
                     <div class="panel-heading" id="databasePanelheading">
                         <h3 class="panel-title">Database</h3>
@@ -61,7 +61,7 @@
                         <img src="./images/ERD.png" class="img-responsive"/>
                     </div>
                 </div>
-                
+
                 <div class="panel panel-primary">
                     <div class="panel-heading" id="ioPanelHeading">
                         <h3 class="panel-title">Legacy Input/Output</h3>
@@ -70,7 +70,7 @@
                         <img src="./images/io_diagram.PNG" class="img-responsive"/>
                     </div>
                 </div>
-				
+
 				<div class="panel panel-primary">
 					<div class="panel-heading" id="algorithmsPanelHeading">
 						<h3 class="panel-title">Algorithms</h3>
@@ -79,7 +79,7 @@
 						<div class="row-fluid">
 
 						<!-- With this grid layout:
-							 Need a xs clearfix every 3 
+							 Need a xs clearfix every 3
 							 Need a sm + md clearfix every 4
 							 Need a lg clearfix every 6 -->
 
@@ -88,13 +88,13 @@
 									<img src="images/ContentDisplay.png">
 								</a>
 							</div>
-					
+
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/ContentEditing.png" class="thumbnail">
 									<img src="images/ContentEditing.png">
 								</a>
 							</div>
-					
+
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/SubmissionApproval.png" class="thumbnail">
 									<img src="images/SubmissionApproval.png">
@@ -102,22 +102,46 @@
 							</div>
 
 							<div class="clearfix visible-xs-block"></div>
-					
+
+              <div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
+                <a href="images/CreateTemplate.png" class="thumbnail">
+                  <img src="images/CreateTemplate.png">
+                </a>
+              </div>
+
+              <div class="clearfix visible-sm-block"></div>
+              <div class="clearfix visible-md-block"></div>
+
+              <div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
+                <a href="images/EditTemplate.png" class="thumbnail">
+                  <img src="images/EditTemplate.png">
+                </a>
+              </div>
+
+              <div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
+                <a href="images/EditTheme.png" class="thumbnail">
+                  <img src="images/EditTheme.png">
+                </a>
+              </div>
+
+              <div class="clearfix visible-xs-block"></div>
+              <div class="clearfix visible-lg-block"></div>
+
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/CommentsDisplay.png" class="thumbnail">
 									<img src="images/CommentsDisplay.png">
 								</a>
 							</div>
 
-							<div class="clearfix visible-sm-block"></div>
-							<div class="clearfix visible-md-block"></div>
-					
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/Favorite-notificationWidget.png" class="thumbnail">
 									<img src="images/Favorite-notificationWidget.png">
 								</a>
 							</div>
-						
+
+              <div class="clearfix visible-sm-block"></div>
+              <div class="clearfix visible-md-block"></div>
+
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/MediaElementDisplay.png" class="thumbnail">
 									<img src="images/MediaElementDisplay.png">
@@ -125,23 +149,19 @@
 							</div>
 
 							<div class="clearfix visible-xs-block"></div>
-							<div class="clearfix visible-lg-block"></div>
-				
+
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/NewsFeedAggregation.png" class="thumbnail">
 									<img src="images/NewsFeedAggregation.png">
 								</a>
 							</div>
-						
+
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/NewsFeedDisplay.png" class="thumbnail">
 									<img src="images/NewsFeedDisplay.png" alt="Comments Display Algorithm">
 								</a>
 							</div>
 
-							<div class="clearfix visible-sm-block"></div>
-							<div class="clearfix visible-md-block"></div>
-							
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/TwitterFeedDisplay.png" class="thumbnail">
 									<img src="images/TwitterFeedDisplay.png" alt="Comments Display Algorithm">
@@ -149,6 +169,9 @@
 							</div>
 
 							<div class="clearfix visible-xs-block"></div>
+              <div class="clearfix visible-sm-block"></div>
+              <div class="clearfix visible-md-block"></div>
+              <div class="clearfix visible-lg-block"></div>
 
 							<div class="col-xs-4 col-sm-3 col-md-3 col-lg-2">
 								<a href="images/TwitterFeedAggregation.png" class="thumbnail">
