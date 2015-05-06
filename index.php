1b8cfd25abe833bbce461fa473c4cc4bbec8388f
ewichern <ewichern@cs.odu.edu>
2015-05-06 16:42:59 -0400

    Updated information on index page, included links to odu.edu and cs.odu.edu.

--- index.php
+++ index.php
@@ -11,20 +11,39 @@
 
 			<div class="default-formatting">
 				<h1>CS 410 Green</h1>
-				<br><br>
-				<div class="panel panel-primary">
-					<div class="panel-heading" id="Overview">
+				<br>
+				</br>
+				<div class="panel panel-primary text-left">
+					<div class="panel-heading" id="overviewTitle">
 						<h3 class="panel-title">Overview</h3>
 					</div>
-					<div class="panel-body" align="left" id="feasibilityBody">
-					<!-- we should add more to this to make it look more full -->
-						<p>As digital journalism and data visualization grows in popularity the technological skillset of undergraduate journalists is inadequate for the demands of university news outlets preparing content for publication in a competitive news arena.</p>
+					<div class="panel-body" id="overviewBody">
+						<!-- we should add more to this to make it look more full -->
+						<p>
+							As journalism becomes an increasingly digital enterprise, and data visualization and interactive become ubiquitous, more diverse and technology-oriented skills are required to create and maintain a competitive, high-quality news publication.
+							University student journalists may find that it only grows more difficult to maintain competency in this wide variety of skills, and the training provided by undergraduate journalism programs often is inadequate in providing the technical skills necessary to compete in the modern, digital news arena.
+						</p>
+						<br>
+						<p>
+							MonarchPress is intended to bridge this gap and provide university news organizations with a tool that will help overcome complex technical hurdles and allow journalists to focus on writing and producing high-quality news content.
+						</p>
+					</div>
+				</div>
+				<div class="panel panel-primary text-left">
+					<div class="panel-heading" id="aboutTitle">
+						<h3 class="panel-title">About</h3>
+					</div>
+					<div class="panel-body" id="aboutBody">
+						<!-- we should add more to this to make it look more full -->
+						<p>
+							This is an undergraduate student project intended as a capstone experience for seniors in <a href="http://odu.edu">Old Dominion University's</a> 
+							<a href="http://www.cs.odu.edu/">Department of Computer Science</a>. More information can be found at: <a href="http://www.cs.odu.edu/~cpi/">Professional Workforce Development - CS410 & CS411</a>
+						</p>
 					</div>
 				</div>
 			</div>
-
 		</div><!-- /.container -->
-				<?php include 'foot.php'; ?>
+		<?php include 'foot.php'; ?>
 
 		<?php require 'scripts.php'; ?>
 	</body>
