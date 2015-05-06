c86f69f7fe4e245e153842e009c98b95dfe05706
ewichern <ewichern@cs.odu.edu>
2015-05-06 16:19:31 -0400

    Added a bunch of references from various other documents.

--- problem.php
+++ problem.php
@@ -18,53 +18,84 @@
 					<p>As digital journalism and data visualization grows in popularity the technological skillset of undergraduate journalists is inadequate for the demands of university news outlets preparing content for publication in a competitive news arena.</p>
 
 					<br>
-					<!-- PROBLEM CHARACTERISITICS -->
-					<div class="panel panel-primary">
+						<!-- PROBLEM CHARACTERISITICS -->
+						<div class="panel panel-primary">
 
-						<div class="panel-heading" id="characteristicsHeading">
-							<h3 class="panel-title" id="problem-characteristics">
-								Problem Characteristics
-							</h3>
+							<div class="panel-heading" id="characteristicsHeading">
+								<h3 class="panel-title" id="problem-characteristics">
+									Problem Characteristics
+								</h3>
+							</div>
+
+							<div class="panel-body greenLists">
+								<ol>
+									<li>University newspapers must maintain existing operations and expand digital presence on limited budgets and face declining revenue from print advertising. Online advertising “cannot make up for the loss of print revenue.” <br>
+											<ul>
+												<li>‘“All newspapers have these problems,” Eric Fidler, the Daily Egyptian’s faculty adviser, said. “Replacing print dollars with digital dollars doesn’t work. You can’t make enough money online to make up for the loss of print revenue.”’    <a href="http://www.stltoday.com/news/local/education/student-newspapers-weigh-trading-independence-for-financial-support/article_29150c79-624f-5527-9fa1-505faa3e5b5b.html">– Student newspapers weigh trading independence for financial support</a></li>
+												<li>“At any given time, the paper employs 30 or more editors, writers, advertising reps, photographers, copy editors and graphic designers. About half of the staff is paid.” <a href="http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html">– Read All About It … In The Student Paper</a></li>
+												<li>“MC with $32,380” – Mace &amp; Crown budget for 2014-2015. <a href="http://www.maceandcrown.com/2014/09/03/sga-balances-600-thousand-in-student-organization-budgets/">– SGA balances behemoth budget</a></li>
+											</ul>
+									</li>
+									<li>Top-tier media outlets are employing entire teams focused on digital journalism and interactive media. To remain competitive in this environment requires a team competent in skills that cross many disciplines. Journalism students do not receive training in complex information/data visualization. <br>
+											<ul>
+												<li>Large media outlets are utilizing entire teams of employees focused on digital and interactive media. <a href="https://www.journalism.co.uk/news/10-key-skills-for-digital-journalists-to-hone-in-2014/s2/a555503/">– 10 key skills for digital journalists to hone in 2014</a></li>
+											</ul>
+									</li>
+									<li>Covering ongoing issues requires living documents that are able to evolve and change over time, to adapt as information and conditions change. <br>
+											<ul>
+												<li>
+													<a href="http://en.wikipedia.org/wiki/Living_document">Wikipedia - Living Document</a>
+												</li>
+											</ul>
+									</li>
+									<li>University newspapers must provide expert reporting on local issues, while also interpreting and providing local perspective on wider - topical, state, national or global - issues. <br>
+											<ul>
+												<li>“Stories about ODU basketball, student government, the state budget and tuition have long been mainstays in the pages of the Mace. Campus parking, as years’ worth of back issues will attest, has always been a popular topic for stories and editorials. The paper also has covered national and international news, particularly in times of war and presidential elections, and has run wire service stories that provide off-campus news and perspectives.” <a href="http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html">– Read All About It … In The Student Paper</a></li>
+											</ul>
+									</li>
+									<li>News outlets and journalists must preserve credibility while conveying newsworthy stories and media to an audience.</li>
+									<li>University newspapers are in a unique position to utilize the “intellectual hub” of the university academic community as a resource while preparing stories for publication. Some university newspapers are unable to utilize these resources because they lack the tools to effectively communicate and collaborate with members of the university community. <br>
+											<ul>
+												<li>
+													<a href="http://www.niemanlab.org/2012/01/the-public-insight-network-now-swimming-in-data-launches-its-own-reporting-unit/">The Public Insight Network, now swimming in data, launches its own reporting unit</a>
+												</li>
+												<li>
+													<a href="http://dangillmor.typepad.com/dan_gillmor_on_grassroots/2005/02/where_newspaper.html">Where Newspapers Can Start the Conversation</a>
+												</li>
+												<li>
+													<a href="http://www.ojr.org/ojr/glaser/1094678265.php">Collaborative Conundrum: Do Wikis Have a Place in the Newsroom?</a>
+												</li>
+												<li>“In one view, there are three types of collaborative citizen journalism: (1) broad-area news; (2) local or regional news; and (3) analytical reporting and “news vetting” of traditional media. Besides independence of thought, collaborative citizen journalism is said to potentially offer expertise not available to the wider-ranging traditional media.” <a href="http://searchsoa.techtarget.com/definition/collaborative-citizen-journalism">– Citizen Collaborative Journalism (CCJ)</a> </li>
+												<li>
+													<a href="http://ec2-23-21-180-28.compute-1.amazonaws.com/mediashift/wp-content/uploads/sites/8/import/i-5608f590c88c0e82640b95434d23dd4b-Planning_bestpractices.pdf">Collective Work: Best Practices for Collaborative Investigative Reporting</a>
+												</li>
+											</ul>
+									</li>
+									<li>The strategic plan for Old Dominion University indicates a variety of goal areas for the university to work towards over the next 5 years. These include the need for real-world opportunities for students, “entrepreneurial interdisciplinary curriculum,” and to increase student and local community engagement. <br>
+											<ul>
+												<li>
+													<a href="https://www.odu.edu/about/planning/strategic-plan-14-19">Strategic Plan (2014-2019)</a>
+												</li>
+											</ul>
+									</li>
+								</ol>
+							</div>
+
 						</div>
 
-						<div class="panel-body greenLists">
-							<ol>
-								<li>University newspapers must maintain existing operations and expand digital presence on limited budgets and face declining revenue from print advertising. Online advertising “cannot make up for the loss of print revenue.” <br>
-									<ul><li>‘“All newspapers have these problems,” Eric Fidler, the Daily Egyptian’s faculty adviser, said. “Replacing print dollars with digital dollars doesn’t work. You can’t make enough money online to make up for the loss of print revenue.”’    <a href="http://www.stltoday.com/news/local/education/student-newspapers-weigh-trading-independence-for-financial-support/article_29150c79-624f-5527-9fa1-505faa3e5b5b.html">– Student newspapers weigh trading independence for financial support</a></li>
-										<li>“At any given time, the paper employs 30 or more editors, writers, advertising reps, photographers, copy editors and graphic designers. About half of the staff is paid.” <a href="http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html">– Read All About It … In The Student Paper</a></li>
-										<li>“MC with $32,380” – Mace &amp; Crown budget for 2014-2015. <a href="http://www.maceandcrown.com/2014/09/03/sga-balances-600-thousand-in-student-organization-budgets/">– SGA balances behemoth budget</a></li></ul></li>
-								<li>Top-tier media outlets are employing entire teams focused on digital journalism and interactive media. To remain competitive in this environment requires a team competent in skills that cross many disciplines. Journalism students do not receive training in complex information/data visualization. <br>
-									<ul><li>Large media outlets are utilizing entire teams of employees focused on digital and interactive media. <a href="https://www.journalism.co.uk/news/10-key-skills-for-digital-journalists-to-hone-in-2014/s2/a555503/">– 10 key skills for digital journalists to hone in 2014</a></li></ul></li>
-								<li>Covering ongoing issues requires living documents that are able to evolve and change over time, to adapt as information and conditions change. <br>
-									<ul><li><a href="http://en.wikipedia.org/wiki/Living_document">Wikipedia - Living Document</a></li></ul></li>
-								<li>University newspapers must provide expert reporting on local issues, while also interpreting and providing local perspective on wider - topical, state, national or global - issues. <br>
-									<ul><li>“Stories about ODU basketball, student government, the state budget and tuition have long been mainstays in the pages of the Mace. Campus parking, as years’ worth of back issues will attest, has always been a popular topic for stories and editorials. The paper also has covered national and international news, particularly in times of war and presidential elections, and has run wire service stories that provide off-campus news and perspectives.” <a href="http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html">– Read All About It … In The Student Paper</a></li></ul></li>
-								<li>News outlets and journalists must preserve credibility while conveying newsworthy stories and media to an audience.</li>
-								<li>University newspapers are in a unique position to utilize the “intellectual hub” of the university academic community as a resource while preparing stories for publication. Some university newspapers are unable to utilize these resources because they lack the tools to effectively communicate and collaborate with members of the university community. <br>
-									<ul><li><a href="http://www.niemanlab.org/2012/01/the-public-insight-network-now-swimming-in-data-launches-its-own-reporting-unit/">The Public Insight Network, now swimming in data, launches its own reporting unit</a></li>
-										<li><a href="http://dangillmor.typepad.com/dan_gillmor_on_grassroots/2005/02/where_newspaper.html">Where Newspapers Can Start the Conversation</a></li>
-										<li><a href="http://www.ojr.org/ojr/glaser/1094678265.php">Collaborative Conundrum: Do Wikis Have a Place in the Newsroom?</a></li>
-										<li>“In one view, there are three types of collaborative citizen journalism: (1) broad-area news; (2) local or regional news; and (3) analytical reporting and “news vetting” of traditional media. Besides independence of thought, collaborative citizen journalism is said to potentially offer expertise not available to the wider-ranging traditional media.” <a href="http://searchsoa.techtarget.com/definition/collaborative-citizen-journalism">– Citizen Collaborative Journalism (CCJ)</a> </li>
-										<li><a href="http://ec2-23-21-180-28.compute-1.amazonaws.com/mediashift/wp-content/uploads/sites/8/import/i-5608f590c88c0e82640b95434d23dd4b-Planning_bestpractices.pdf">Collective Work: Best Practices for Collaborative Investigative Reporting</a></li></ul></li>
-								<li>The strategic plan for Old Dominion University indicates a variety of goal areas for the university to work towards over the next 5 years. These include the need for real-world opportunities for students, “entrepreneurial interdisciplinary curriculum,” and to increase student and local community engagement. <br>
-									<ul><li><a href="https://www.odu.edu/about/planning/strategic-plan-14-19">Strategic Plan (2014-2019)</a></li></ul></li>
-							</ol>
-						</div>
+						<div class="panel panel-primary">
+							<div class="panel-heading">
+								<h3 class="panel-title">
+									Process Flow Diagram
+								</h3>
+							</div>
+							<div class="panel-body" align="center">
+								<img src="./images/new_current_process.png" class="img-responsive"/>
+								<br/>
 
-					</div>
-
-					<div class="panel panel-primary">
-						<div class="panel-heading">
-							<h3 class="panel-title">
-								Process Flow Diagram
-							</h3>
+								<p class="text-left">The current process flow diagram needs software to intervene at two separate stages. The first is when content is being aggregated for a story. University newspapers could stand to have better social media integration for the stories uploaded to their websites. This would mean utilizing guest columnists, whitelisted twitter feeds, and reputation based forums for users to engage in discussion. Lastly, software intervention is needed before stories are pushed to university newspaper websites. Due to the lacking technological skillset of undergraduate journalists, content uploaded isn’t captivating enough to preserve the attention of readers. Adjustments to the aesthetic of these stories as well as data visualization to accompany those changes is needed.</p>
+							</div>
 						</div>
-						<div class="panel-body" align="center">
-							<img src="./images/new_current_process.png" class="img-responsive"/><br/>
-
-							<p class="text-left">The current process flow diagram needs software to intervene at two separate stages. The first is when content is being aggregated for a story. University newspapers could stand to have better social media integration for the stories uploaded to their websites. This would mean utilizing guest columnists, whitelisted twitter feeds, and reputation based forums for users to engage in discussion. Lastly, software intervention is needed before stories are pushed to university newspaper websites. Due to the lacking technological skillset of undergraduate journalists, content uploaded isn’t captivating enough to preserve the attention of readers. Adjustments to the aesthetic of these stories as well as data visualization to accompany those changes is needed.</p>
-						</div>
-					</div>
 
 				</div>
 			</div>
--- references.php
+++ references.php
@@ -12,22 +12,92 @@
 			<div class="default-formatting">
 				<h1>References</h1>
 				<div class="text-left">
-					Environmental Protection Agency - Climate Change: Basic Information</br>
-					<a href="http://www.epa.gov/climatechange/basics/">http://www.epa.gov/climatechange/basics</a></br></br>
-					2014 National Climate Assessment</br>
-					<a href="http://nca2014.globalchange.gov/report">http://nca2014.globalchange.gov/report</a></br></br>
-					Natural Resources Defense Council - Climate Change Health Threats in Virginia</br>
-					<a href="http://www.nrdc.org/health/climate/va.asp">http://www.nrdc.org/health/climate/va.asp</a></br></br>
-					Washington Post - In Norfolk, evidence of climate change is in the streets at high tide</br>
-					<a href="http://www.washingtonpost.com/business/economy/in-norfolk-evidence-of-climate-change-is-in-the-streets-at-high-tide/2014/05/31/fe3ae860-e71f-11e3-8f90-73e071f3d637_story.html">http://www.washingtonpost.com/business/economy/in-norfolk-evidence-of-climate-change-is-in-the-streets-at-high-tide/2014/05/31/fe3ae860-e71f-11e3-8f90-73e071f3d637_story.html</a></br></br>
-					NPR - As Sea Levels Rise, Norfolk Is Sinking And Planning</br>
-					<a href="http://www.npr.org/2014/06/24/324891517/as-sea-levels-rise-norfolk-is-sinking-and-planning">http://www.npr.org/2014/06/24/324891517/as-sea-levels-rise-norfolk-is-sinking-and-planning</a></br></br>
-					USA Today - Rising sea levels torment Norfolk, Va., and coastal U.S.</br>
-					<a href="http://www.usatoday.com/story/news/nation/2013/12/17/sea-level-rise-swamps-norfolk-us-coasts/3893825/">http://www.usatoday.com/story/news/nation/2013/12/17/sea-level-rise-swamps-norfolk-us-coasts/3893825/</a></br></br>
-					Huffington Post - Norfolk, Virginia's Rising Sea Levels And Sinking Ground Pose A Community Challenge</br>
-					<a href="http://www.huffingtonpost.com/2012/04/28/norfolk-virginia-sea-levels-rise_n_1461486.html">http://www.huffingtonpost.com/2012/04/28/norfolk-virginia-sea-levels-rise_n_1461486.html</a></br></br>
-					The Daily Climate - Sea-level rise: Whatever you call it, water is rising in Norfolk</br>
-					<a href="http://www.dailyclimate.org/tdc-newsroom/2012/08/norfolk-sea-level-rise">http://www.dailyclimate.org/tdc-newsroom/2012/08/norfolk-sea-level-rise</a></br></br>
+					Student newspapers weigh trading independence for financial support<br>
+					<a href="http://www.stltoday.com/news/local/education/student-newspapers-weigh-trading-independence-for-financial-support/article_29150c79-624f-5527-9fa1-505faa3e5b5b.html">http://www.stltoday.com/news/local/education/student-newspapers-weigh-trading-independence-for-financial-support/article_29150c79-624f-5527-9fa1-505faa3e5b5b.html</a><br><br>
+					Read All About It … In The Student Paper<br>
+					<a href="http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html">http://ww2.odu.edu/ao/alumni_magazine/fall03/Deadlines%20and%20Headlines.html</a><br><br>
+					SGA balances behemoth budget<br>
+					<a href="http://www.maceandcrown.com/2014/09/03/sga-balances-600-thousand-in-student-organization-budgets/">http://www.maceandcrown.com/2014/09/03/sga-balances-600-thousand-in-student-organization-budgets/</a><br><br>
+					10 key skills for digital journalists to hone in 2014<br>
+					<a href="https://www.journalism.co.uk/news/10-key-skills-for-digital-journalists-to-hone-in-2014/s2/a555503/">https://www.journalism.co.uk/news/10-key-skills-for-digital-journalists-to-hone-in-2014/s2/a555503/</a><br><br>
+					Wikipedia - Living Document<br>
+					<a href="http://en.wikipedia.org/wiki/Living_document">http://en.wikipedia.org/wiki/Living_document</a><br><br>
+					The Public Insight Network, now swimming in data, launches its own reporting unit<br>
+					<a href="http://www.niemanlab.org/2012/01/the-public-insight-network-now-swimming-in-data-launches-its-own-reporting-unit/">http://www.niemanlab.org/2012/01/the-public-insight-network-now-swimming-in-data-launches-its-own-reporting-unit/</a><br><br>
+					Where Newspapers Can Start the Conversation<br>
+					<a href="http://dangillmor.typepad.com/dan_gillmor_on_grassroots/2005/02/where_newspaper.html">http://dangillmor.typepad.com/dan_gillmor_on_grassroots/2005/02/where_newspaper.html</a><br><br>
+					Collaborative Conundrum: Do Wikis Have a Place in the Newsroom?<br>
+					<a href="http://www.ojr.org/ojr/glaser/1094678265.php">http://www.ojr.org/ojr/glaser/1094678265.php</a><br><br>
+					Citizen Collaborative Journalism (CCJ)<br>
+					<a href="http://searchsoa.techtarget.com/definition/collaborative-citizen-journalism">http://searchsoa.techtarget.com/definition/collaborative-citizen-journalism</a><br><br>
+					Collective Work: Best Practices for Collaborative Investigative Reporting<br>
+					<a href="http://ec2-23-21-180-28.compute-1.amazonaws.com/mediashift/wp-content/uploads/sites/8/import/i-5608f590c88c0e82640b95434d23dd4b-Planning_bestpractices.pdf">http://ec2-23-21-180-28.compute-1.amazonaws.com/mediashift/wp-content/uploads/sites/8/import/i-5608f590c88c0e82640b95434d23dd4b-Planning_bestpractices.pdf</a><br><br>
+					ODU Strategic Plan (2014-2019)<br>
+					<a href="https://www.odu.edu/about/planning/strategic-plan-14-19">https://www.odu.edu/about/planning/strategic-plan-14-19</a><br><br>
+					
+					GoDaddy.com managed WordPress hosting<br>
+					<a href="https://www.godaddy.com/pro/managed-wordpress-hosting">https://www.godaddy.com/pro/managed-wordpress-hosting</a><br><br>
+			
+					Wordpress Requirements<br>
+					<a href="https://wordpress.org/about/requirements/">https://wordpress.org/about/requirements/</a><br><br>
+						
+					Wordpress usage statistics<br>
+					<a href="https://wordpress.org/about/stats/">https://wordpress.org/about/stats/</a><br><br>
+					
+					WordPress APIs<br>
+					<a href="https://codex.wordpress.org/WordPress_APIs">https://codex.wordpress.org/WordPress_APIs</a><br><br>
+					
+					WordPress Social Login<br>
+					<a href="https://wordpress.org/plugins/wordpress-social-login">https://wordpress.org/plugins/wordpress-social-login</a><br><br>
+												
+					4 ways content management systems are evolving & why it matters to journalists<br>
+					<a href="http://www.poynter.org/news/media-innovation/134791/4-ways-content-management-systems-are-evolving-why-it-matters-to-journalists/">http://www.poynter.org/news/media-innovation/134791/4-ways-content-management-systems-are-evolving-why-it-matters-to-journalists/</a><br><br>
+						
+					Pangea CMS<br>
+					<a href="http://www.pangea-cms.com/">http://www.pangea-cms.com/</a><br><br>
+						
+					Newscoop<br>
+					<a href="https://www.sourcefabric.org/en/newscoop/">https://www.sourcefabric.org/en/newscoop/</a><br><br>
+						
+					Armstrong CMS<br>
+					<a href="http://www.armstrongcms.org/">http://www.armstrongcms.org/</a><br><br>
+						
+					Django CMS<br>
+					<a href="https://www.django-cms.org/en/">https://www.django-cms.org/en/</a><br><br>
+					
+					CMS Market Share<br>
+					<a href="http://www.opensourcecms.com/general/cms-marketshare.php">http://www.opensourcecms.com/general/cms-marketshare.php</a><br><br>	
+					
+					Agile Manifesto<br>
+					<a href="http://agilemanifesto.org/">http://agilemanifesto.org/</a><br><br>
+					
+					Twitter Search API<br>
+					<a href="https://dev.twitter.com/rest/public/search">https://dev.twitter.com/rest/public/search</a><br><br>
+					
+					OAuth Documentation<br>
+					<a href="http://oauth.net/documentation/">http://oauth.net/documentation/</a><br><br>
+					
+					<!--
+					<br>
+					<a href=""></a><br><br>
+					-->							
+						
+					Environmental Protection Agency - Climate Change: Basic Information<br>
+					<a href="http://www.epa.gov/climatechange/basics/">http://www.epa.gov/climatechange/basics</a><br><br>
+					2014 National Climate Assessment<br>
+					<a href="http://nca2014.globalchange.gov/report">http://nca2014.globalchange.gov/report</a><br><br>
+					Natural Resources Defense Council - Climate Change Health Threats in Virginia<br>
+					<a href="http://www.nrdc.org/health/climate/va.asp">http://www.nrdc.org/health/climate/va.asp</a><br><br>
+					Washington Post - In Norfolk, evidence of climate change is in the streets at high tide<br>
+					<a href="http://www.washingtonpost.com/business/economy/in-norfolk-evidence-of-climate-change-is-in-the-streets-at-high-tide/2014/05/31/fe3ae860-e71f-11e3-8f90-73e071f3d637_story.html">http://www.washingtonpost.com/business/economy/in-norfolk-evidence-of-climate-change-is-in-the-streets-at-high-tide/2014/05/31/fe3ae860-e71f-11e3-8f90-73e071f3d637_story.html</a><br><br>
+					NPR - As Sea Levels Rise, Norfolk Is Sinking And Planning<br>
+					<a href="http://www.npr.org/2014/06/24/324891517/as-sea-levels-rise-norfolk-is-sinking-and-planning">http://www.npr.org/2014/06/24/324891517/as-sea-levels-rise-norfolk-is-sinking-and-planning</a><br><br>
+					USA Today - Rising sea levels torment Norfolk, Va., and coastal U.S.<br>
+					<a href="http://www.usatoday.com/story/news/nation/2013/12/17/sea-level-rise-swamps-norfolk-us-coasts/3893825/">http://www.usatoday.com/story/news/nation/2013/12/17/sea-level-rise-swamps-norfolk-us-coasts/3893825/</a><br><br>
+					Huffington Post - Norfolk, Virginia's Rising Sea Levels And Sinking Ground Pose A Community Challenge<br>
+					<a href="http://www.huffingtonpost.com/2012/04/28/norfolk-virginia-sea-levels-rise_n_1461486.html">http://www.huffingtonpost.com/2012/04/28/norfolk-virginia-sea-levels-rise_n_1461486.html</a><br><br>
+					The Daily Climate - Sea-level rise: Whatever you call it, water is rising in Norfolk<br>
+					<a href="http://www.dailyclimate.org/tdc-newsroom/2012/08/norfolk-sea-level-rise">http://www.dailyclimate.org/tdc-newsroom/2012/08/norfolk-sea-level-rise</a><br><br>
 				</div>
 
 
